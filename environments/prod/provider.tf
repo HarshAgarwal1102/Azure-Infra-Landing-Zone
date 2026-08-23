10bd79247ece62bae704af69c83891ae38a7d68d
Harsh Agarwal <agarwal_harsh@MacBook-Air.local>
2026-08-23 19:43:06 +0530

    added Storate Account

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,6 +6,12 @@ terraform {
       version = "4.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstatedatastore"
+    container_name       = "tfstate"
+    key                  = "preprod.tfstate"
+  }
 }
 
 provider "azurerm" {
